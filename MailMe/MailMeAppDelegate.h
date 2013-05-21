305c310d222262097c3c946087499dddd2cc1674
Sean Dawson <sean.dawson@gmail.com>
2013-05-21 10:28:45 -0400

    Added License/Readme, downgraded code signing to iPhone developer (auto selector)

--- MailMe/MailMeAppDelegate.h
+++ MailMe/MailMeAppDelegate.h
@@ -5,6 +5,9 @@
 //  Created by Sean Dawson on 2013-05-14.
 //  Copyright (c) 2013 Sean Dawson. All rights reserved.
 //
+//  This file is released under the MIT License:
+//  http://www.opensource.org/licenses/mit-license.php
+//
 
 #ifndef _MailMeAppDelegate_h
 #define _MailMeAppDelegate_h
--- MailMe/MailMeAppDelegate.m
+++ MailMe/MailMeAppDelegate.m
@@ -5,6 +5,9 @@
 //  Created by Sean Dawson on 2013-05-14.
 //  Copyright (c) 2013 Sean Dawson. All rights reserved.
 //
+//  This file is released under the MIT License:
+//  http://www.opensource.org/licenses/mit-license.php
+//
 
 #import "MailMeAppDelegate.h"
 
